b9327a447634f9f07ad2cbeb89e576f1707cb537
Sabir934 <162897400+Sabir934@users.noreply.github.com>
2025-04-23 10:14:05 +0530

    Create Task 2.py
    update

---
+++ Task 2.py
@@ -0,0 +1,17 @@
+#1.create a list of number from 1 to 10
+#2.extract the first five elements from the list
+#3.Reverses these extracted element.
+#4.print both the extracted list and the reversed list.
+
+Orignal_list=list(range(1,11))
+my_five=Orignal_list[:5] #extracted first five elements using del
+print("Orignal list: ",Orignal_list)
+print("Extracted first five elements:",my_five)
+print("Reversed extracted elements :",my_five[::-1])
+
+
+
+
+
+
+
